3800609663bb4bc668e34355b8bd30fa8ae4aa20
Duvarapu Rushitha <rushithaduvarapu@gmail.com>
2026-03-21 16:50:11 +0530

    connected users route to database

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-// GET users
-router.get("/", (req, res) => {
-  res.json({ message: "Users from separate route file 🚀" });
+// GET all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
